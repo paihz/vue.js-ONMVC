e861c991aa92f6bdab5f446d187ee3d3dfcac750
paihz <mohdfaihz@gmail.com>
2015-06-24 20:42:47 +0800

    two way binding pages

---
+++ js/2waybinding.js
@@ -0,0 +1,17 @@
+new Vue({
+	el: '#demo',
+
+	data: {
+		name: 'Change this'
+	},
+
+	ready:  function() {
+		var that = this;
+
+		setInterval(function() {
+			that.name = 'Updated'; 
+		}, 5000);
+
+	}
+
+});
